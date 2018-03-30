fca350d4535d656c387d321cd88ec13afc764e63
Jelle Spekken <jelle.spekken@centagon.com>
2018-03-30 09:14:40 +0200

    es6-ified code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-module.exports = function (value) {
-    var regex = /^(0?[-.]?\d+)(r?e[m|x]|v[h|w|min|max]+|p[x|t|c]|[c|m]m|%|s|in|ch)$/;
-    var match = value.match(regex);
+module.exports = (value) => {
+    const regex = /^(0?[-.]?\d+)(r?e[m|x]|v[h|w|min|max]+|p[x|t|c]|[c|m]m|%|s|in|ch)$/;
+    const match = value.match(regex);
 
     return match
         ? { value: (parseFloat(match[1]) || match[1]), unit: match[2] }
