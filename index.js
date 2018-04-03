d02be9393c06f9aaf63de47b47c8b4e10ec6978d
Jelle Spekken <jelle.spekken@centagon.com>
2018-04-03 09:09:38 +0200

    minor code flow cleanup

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 module.exports = (value, onlyReturnUnit = false) => {
-    const regex = /^(0?[-.]?\d+)(r?e[m|x]|v[h|w|min|max]+|p[x|t|c]|[c|m]m|%|s|in|ch)$/;
-    const match = value.match(regex);
-
-    if (onlyReturnUnit === true) {
-        return match ? match[2] : undefined;
-    }
-
-    return match
+    const match = value.match(/^(0?[-.]?\d+)(r?e[m|x]|v[h|w|min|max]+|p[x|t|c]|[c|m]m|%|s|in|ch)$/);
+    const res = match
         ? { value: (parseFloat(match[1]) || match[1]), unit: match[2] }
         : { value, unit: undefined };
+
+    return onlyReturnUnit ? res.unit : res;
 };
